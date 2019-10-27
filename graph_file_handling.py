Summary ROUGE score calculation and some fixes

graph_file_handling.py
@@ -112,21 +112,29 @@ def get_first_batch_graph_dict(path, batch_size, keep_features, existence_as_vec
     return list_of_dicts
 
 
-def save_predicted_graphs(path, inputs, outputs):
+def save_predicted_graphs(path, inputs, outputs, accurately=False):
     """
     Saves the predicted graphs to a jsonl file
     :param path: The path where the file shall be saved
     :param inputs: Training input graphs
     :param outputs: Training output graphs
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     """
     inputs_dict = utils_np.graphs_tuple_to_data_dicts(inputs)
     outputs_dict = utils_np.graphs_tuple_to_data_dicts(outputs)
     mode = 'a' if os.path.exists(path) else 'w'
     with open(path, mode) as output:
         for (in_, out_) in zip(inputs_dict, outputs_dict):
-            out_dict = {"nodes": [[i[0], int(np.argmax(o))] for (i, o) in zip(in_["nodes"], out_["nodes"])],
-                        "edges": [[i[0], int(np.argmax(o))] for (i, o) in zip(in_["edges"], out_["edges"])],
-                        "globals": [float(g) for g in in_["globals"]],
-                        "senders": in_["senders"].tolist(),
-                        "receivers": in_["receivers"].tolist()}
+            if not accurately:
+                out_dict = {"nodes": [[i.tolist(), int(np.argmax(o))] for (i, o) in zip(in_["nodes"], out_["nodes"])],
+                            "edges": [[i[0], int(np.argmax(o))] for (i, o) in zip(in_["edges"], out_["edges"])],
+                            "globals": [float(g) for g in in_["globals"]],
+                            "senders": in_["senders"].tolist(),
+                            "receivers": in_["receivers"].tolist()}
+            else:
+                out_dict = {"nodes": [[i.tolist(), o[1]] for (i, o) in zip(inputs_dict["nodes"], outputs_dict["nodes"])],
+                            "edges": [[i[0], o[1]] for (i, o) in zip(inputs_dict["edges"], outputs_dict["edges"])],
+                            "globals": [float(g) for g in inputs_dict["globals"]],
+                            "senders": inputs_dict["senders"].tolist(),
+                            "receivers": inputs_dict["receivers"].tolist()}
             print(json.dumps(out_dict), file=output)

main.py
@@ -56,7 +56,7 @@ def preprocess(models_dir, processors, extractive, cnn_dm_file, article_file, su
 
 
 def train(model, model_path, save_prediction, valid_files, use_gpu, use_edges, epoch, batch_size,
-          training_steps_per_epoch, validation_steps_per_epoch, train_files, print_steps, early_stopping):
+          training_steps_per_epoch, validation_steps_per_epoch, train_files, print_steps, early_stopping, accurately):
     """
     Trains the model given as the parameter using a data generator
     :param model: The model to train
@@ -72,6 +72,7 @@ def train(model, model_path, save_prediction, valid_files, use_gpu, use_edges, e
     :param train_files: The paths of the files used for training
     :param print_steps: Whether to print each batch's report
     :param early_stopping: Whether to use Early stopping
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     """
     import tensorflow as tf
     from graph_transformations.network import train_generator
@@ -97,10 +98,11 @@ def train(model, model_path, save_prediction, valid_files, use_gpu, use_edges, e
                     validation_steps_per_epoch=validation_steps_per_epoch, inputs_train_file=train_files[0],
                     outputs_train_file=train_files[1], inputs_test_file=valid_files[0],
                     outputs_test_file=valid_files[1], output_save_path=save_prediction, save_model_path=model_path,
-                    use_edges=use_edges, device=device, print_steps=print_steps, early_stopping=early_stopping)
+                    accurately=accurately, use_edges=use_edges, device=device, print_steps=print_steps,
+                    early_stopping=early_stopping)
 
 
-def test(model, model_path, save_prediction, valid_files, use_gpu, use_edges, batch_size, print_steps):
+def test(model, model_path, save_prediction, valid_files, use_gpu, use_edges, batch_size, print_steps, accurately):
     """
     Tests the model restored from model path with given test data
     :param model: The model to train
@@ -111,6 +113,7 @@ def test(model, model_path, save_prediction, valid_files, use_gpu, use_edges, ba
     :param use_edges: Whether or not to train on the edges as well as the nodes
     :param batch_size: The size of a batch
     :param print_steps: Whether to print each batch's report
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     """
     import tensorflow as tf
     from graph_transformations.network import test
@@ -131,12 +134,13 @@ def test(model, model_path, save_prediction, valid_files, use_gpu, use_edges, ba
         device = "/device:CPU:0"
 
     tf.reset_default_graph()
-    test(model=model, checkpoint=model_path, input_data=valid_files[0], target_data=valid_files[1],
-         batch_size=batch_size, output_save_path=save_prediction, use_edges=use_edges,
+    model_to_test = Model(edge_output_size=2, node_output_size=2, global_output_size=1)
+    test(model=model_to_test, checkpoint=model_path, input_data=valid_files[0], target_data=valid_files[1],
+         batch_size=batch_size, output_save_path=save_prediction, accurately=accurately, use_edges=use_edges,
          device=device, print_steps=print_steps)
 
 
-def predict(model, model_path, save_prediction, input_file, use_gpu, batch_size):
+def predict(model, model_path, save_prediction, input_file, use_gpu, batch_size, accurately):
     """
     Predicts the output of the model restored from model path with given data
     :param model: The model to train
@@ -145,6 +149,7 @@ def predict(model, model_path, save_prediction, input_file, use_gpu, batch_size)
     :param input_file: The path of the input file used for prediction
     :param use_gpu: Which device to use
     :param batch_size: The size of a batch
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     """
     import tensorflow as tf
     from graph_transformations.network import predict
@@ -161,8 +166,9 @@ def predict(model, model_path, save_prediction, input_file, use_gpu, batch_size)
         device = "/device:CPU:0"
 
     tf.reset_default_graph()
-    predict(model=model, checkpoint=model_path, data=input_file, batch_size=batch_size,
-            output_save_path=save_prediction, device=device)
+    model_to_predict = Model(edge_output_size=2, node_output_size=2, global_output_size=1)
+    predict(model=model_to_predict, checkpoint=model_path, data=input_file, batch_size=batch_size,
+            output_save_path=save_prediction, accurately=accurately, device=device)
 
 
 def visualize(file_path, line_number, save_image, all_displayed, color_ones, use_edges):
@@ -212,11 +218,12 @@ if __name__ == "__main__":
     elif args.mode == "train":
         train(args.model, args.model_path, args.save_prediction, args.valid_files, args.use_gpu, args.use_edges,
               args.epoch, args.batch_size, args.training_steps_per_epoch, args.validation_steps_per_epoch,
-              args.train_files, args.print_steps, not args.no_early_stopping)
+              args.train_files, args.print_steps, not args.no_early_stopping, args.accurately)
     elif args.mode == "test":
         test(args.model, args.model_path, args.save_prediction, args.valid_files, args.use_gpu, args.use_edges,
-             args.batch_size, args.print_steps)
+             args.batch_size, args.print_steps, args.accurately)
     elif args.mode == "predict":
-        predict(args.model, args.model_path, args.save_prediction, args.input_file, args.use_gpu, args.batch_size)
+        predict(args.model, args.model_path, args.save_prediction, args.input_file, args.use_gpu, args.batch_size,
+                args.accurately)
     elif args.mode == "visualize":
         visualize(args.file_path, args.line, args.save_image, args.all_displayed, args.color_ones, args.use_edges)

network.py
@@ -1,12 +1,13 @@
-from graph_nets import utils_tf
+from graph_nets import utils_tf, utils_np
 from graph_nets.demos.models import EncodeProcessDecode
 import tensorflow as tf
 import os
 
 from graph_transformations.models.model_with_attention import GraphAttention
 from graph_transformations.graph_losses import softmax_loss, softmax_loss_on_nodes
-from graph_transformations.graph_file_handling import get_first_batch_graph_dict, generate_graph, save_predicted_graphs
-from graph_transformations.compute_measures import compute_accuracy, compute_accuracy_on_nodes,\
+from graph_transformations.graph_file_handling import get_first_batch_graph_dict, generate_graph, \
+                                                      save_predicted_graphs, process_line
+from graph_transformations.compute_measures import compute_accuracy, compute_accuracy_on_nodes, \
                                                    compute_tp_tn_fp_fn, add_tp_tn_fp_fn, compute_precision_recall_f1
 
 
@@ -68,13 +69,13 @@ def train_model(model, epochs, inputs_train, targets_train, inputs_test, targets
                 train_values["loss"], correct_train, solved_train))
             print("Test loss: {}\tCorrect test parts: {}\tCorrectly solved test graphs: {}".format(
                 test_values["loss"], correct_test, solved_test))
-        save_predicted_graphs(output_save_path, train_values["inputs"], train_values["outputs"][-1],
-                              test_values["inputs"], test_values["outputs"][-1])
+        save_predicted_graphs(output_save_path, train_values["inputs"], train_values["outputs"][-1])
+        save_predicted_graphs(output_save_path, test_values["inputs"], test_values["outputs"][-1])
 
 
 def run_session(sess, inputs, input_ph, outputs, steps_per_epoch,
                 use_edges=None, targets=None, target_ph=None, optimize=None, loss=None, print_steps=None,
-                output_save_path=None):
+                output_save_path=None, accurately=False):
     """
     Runs training, testing or prediction in session
     :param sess: The tensorflow session for running the training testing or prediction.
@@ -89,6 +90,7 @@ def run_session(sess, inputs, input_ph, outputs, steps_per_epoch,
     :param loss: Loss function
     :param print_steps: Whether to print each batch's report
     :param output_save_path: The path where the final output graphs shall be saved
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     :return: The function returns the current loss, if it could be calculated
     """
     losses = []
@@ -131,7 +133,7 @@ def run_session(sess, inputs, input_ph, outputs, steps_per_epoch,
         values = sess.run(sess_dict, feed_dict=feed_dict)
 
         if output_save_path is not None:
-            save_predicted_graphs(output_save_path, values["inputs"], values["outputs"][-1])
+            save_predicted_graphs(output_save_path, values["inputs"], values["outputs"][-1], accurately)
 
         if targets is not None:
             correct, solved_ = compute_accuracy(values["targets"], values["outputs"][-1]) if use_edges \
@@ -168,7 +170,7 @@ def run_session(sess, inputs, input_ph, outputs, steps_per_epoch,
 
 def train_generator(model, epochs, batch_size, steps_per_epoch, validation_steps_per_epoch,
                     inputs_train_file, outputs_train_file,
-                    inputs_test_file, outputs_test_file, output_save_path, save_model_path,
+                    inputs_test_file, outputs_test_file, output_save_path, save_model_path, accurately,
                     use_edges=True, device='/device:GPU:0', print_steps=False, early_stopping=True):
     """
     Trains the model given as the parameter using a data generator
@@ -183,6 +185,7 @@ def train_generator(model, epochs, batch_size, steps_per_epoch, validation_steps
     :param outputs_test_file: The path of the target file used for testing
     :param output_save_path: The path where the final output graphs shall be saved
     :param save_model_path: Where to save the trained model
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     :param use_edges: Whether or not to train on the edges as well as the nodes
     :param device: Which device to use. GPU:0 is the default.
     :param print_steps: Whether to print each batch's report
@@ -249,13 +252,14 @@ def train_generator(model, epochs, batch_size, steps_per_epoch, validation_steps
                 test_len = int(len(open(inputs_test_file).read().split('\n')) / batch_size_)
                 run_session(sess, inputs_train, input_train_ph, output_train, train_len,
                             output_save_path=output_save_path)
-                run_session(sess, inputs_test, input_test_ph, output_test, test_len, output_save_path=output_save_path)
+                run_session(sess, inputs_test, input_test_ph, output_test, test_len, output_save_path=output_save_path,
+                            accurately=accurately)
             save_path = saver.save(sess, "{}.ckpt".format(save_model_path))
             print("Model saved in path: %s" % save_path)
     return model
 
 
-def predict(model, checkpoint, data, batch_size, output_save_path, device='/device:GPU:0'):
+def predict(model, checkpoint, data, batch_size, output_save_path, accurately, device='/device:GPU:0'):
     """
     Use the model to predict the output with given input
     :param model: The model used for prediction
@@ -263,6 +267,7 @@ def predict(model, checkpoint, data, batch_size, output_save_path, device='/devi
     :param data: The path of the input file used for prediction
     :param batch_size: The size of a batch
     :param output_save_path: The path where the final output graphs shall be saved
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     :param device: Which device to use. GPU:0 is the default.
     """
     inputs = generate_graph(data, batch_size, keep_features=True)
@@ -277,10 +282,38 @@ def predict(model, checkpoint, data, batch_size, output_save_path, device='/devi
             checkpoint = checkpoint if checkpoint.endswith(".ckpt") else "{}.ckpt".format(checkpoint)
             saver.restore(sess, checkpoint)
             steps = int(len(open(data).read().split('\n')) / batch_size_)
-            run_session(sess, inputs, input_ph, outputs, steps, output_save_path=output_save_path)
+            run_session(sess, inputs, input_ph, outputs, steps, output_save_path=output_save_path, accurately=accurately)
 
 
-def test(model, checkpoint, input_data, target_data, batch_size, output_save_path, use_edges=True,
+def predict_one_graph(model, checkpoint, json_data, device='/device:GPU:0'):
+    """
+    Predicts one output for the one given input
+    :param model: The model used for prediction
+    :param checkpoint: The path to the previously saved checkpoint file
+    :param json_data:
+    :param device: Which device to use. GPU:0 is the default.
+    :return: The scored ooutput
+    """
+    data_dict_list = [process_line(json_dict=json_data, keep_features=True, existence_as_vector=False)]
+    inputs = utils_tf.data_dicts_to_graphs_tuple(data_dict_list)
+    outputs = model(inputs, 1)
+    saver = tf.train.Saver()
+    with tf.device(device):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            checkpoint = checkpoint if checkpoint.endswith(".ckpt") else "{}.ckpt".format(checkpoint)
+            saver.restore(sess, checkpoint)
+            values = sess.run({"inputs": inputs, "outputs": outputs})
+    inputs_dict = utils_np.graphs_tuple_to_data_dicts(values["inputs"])[0]
+    outputs_dict = utils_np.graphs_tuple_to_data_dicts(values["outputs"][0])[0]
+    return {"nodes": [[i.tolist(), o[1]] for (i, o) in zip(inputs_dict["nodes"], outputs_dict["nodes"])],
+            "edges": [[i[0], o[1]] for (i, o) in zip(inputs_dict["edges"], outputs_dict["edges"])],
+            "globals": [float(g) for g in inputs_dict["globals"]],
+            "senders": inputs_dict["senders"].tolist(),
+            "receivers": inputs_dict["receivers"].tolist()}
+
+
+def test(model, checkpoint, input_data, target_data, batch_size, output_save_path, accurately, use_edges=True,
          device='/device:GPU:0', print_steps=False):
     """
     Tests the model at the checkpoint.
@@ -290,6 +323,7 @@ def test(model, checkpoint, input_data, target_data, batch_size, output_save_pat
     :param target_data: The path of the target file used for testing
     :param batch_size: The size of a test batch
     :param output_save_path: The path where the final output graphs shall be saved
+    :param accurately: Whether to save the scores or just whether the result was one or zero
     :param use_edges: Whether or not to test on the edges as well as the nodes
     :param device: Which device to use. GPU:0 is the default.
     :param print_steps: Whether to print each batch's report
@@ -314,7 +348,8 @@ def test(model, checkpoint, input_data, target_data, batch_size, output_save_pat
             saver.restore(sess, checkpoint)
             steps = int(len(open(input_data).read().split('\n')) / batch_size_)
             run_session(sess, inputs, input_ph, outputs, steps, targets=targets, target_ph=target_ph, loss=loss,
-                        use_edges=use_edges, print_steps=print_steps, output_save_path=output_save_path)
+                        use_edges=use_edges, print_steps=print_steps, output_save_path=output_save_path,
+                        accurately=accurately)
 
 
 if __name__ == '__main__':
@@ -325,13 +360,13 @@ if __name__ == '__main__':
     epochs_ = 2
     batch_size_ = 8
 
-    # encode_process_decode_model = EncodeProcessDecode(edge_output_size=2, node_output_size=2, global_output_size=1)
+    encode_process_decode_model = EncodeProcessDecode(edge_output_size=2, node_output_size=2, global_output_size=1)
     graph_dependent_model = GraphAttention(edge_output_size=2, node_output_size=2, global_output_size=1)
 
     training_steps = int(len(open('./data/sentences_train2.jsonl').read().split('\n')) / batch_size_)
     validation_steps = int(len(open('./data/sentences_test2.jsonl').read().split('\n')) / batch_size_)
 
-    train_generator(graph_dependent_model, epochs_, batch_size_, training_steps, validation_steps,
+    """train_generator(graph_dependent_model, epochs_, batch_size_, training_steps, validation_steps,
                     './data/sentences_train3.jsonl',
                     './data/highlight_sentences_train3.jsonl',
                     './data/sentences_test3.jsonl',
@@ -340,12 +375,12 @@ if __name__ == '__main__':
                     './chkpt/model_checkpoint',
                     use_edges=False,
                     print_steps=True,
-                    device='/device:CPU:0')
+                    device='/device:CPU:0')"""
 
-    """predict(graph_dependent_model, './chkpt/model_checkpoint', './data/sentences_test2.jsonl', batch_size_,
+    predict(graph_dependent_model, './chkpt4/model_checkpoint', './data/sentences2.jsonl', batch_size_,
             "./data/tmp/pred.jsonl", device='/device:CPU:0')
 
-    test(graph_dependent_model, './chkpt/model_checkpoint', './data/sentences_test2.jsonl',
+    """test(graph_dependent_model, './chkpt/model_checkpoint', './data/sentences_test2.jsonl',
          './data/highlight_sentences_test2.jsonl', batch_size_, "./data/tmp/pred2.jsonl", use_edges=False,
          device='/device:CPU:0')"""
 